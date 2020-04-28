a50a2c14b6ffea51469de99e4a45a246c89d45af
Hannah Riley <riley.hannahm@gmail.com>
2020-04-28 16:01:19 -0700

    updated user container

--- src/containers/searchUser/UserContainer.jsx
+++ src/containers/searchUser/UserContainer.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import SearchUser from '../../components/SearchUser.jsx';
-import UserDisplay from '../../components/UserDisplay.jsx';
-import ColorDisplay from '../../components/ColorDisplay/ColorDisplay.jsx';
+import RepoList from '../../components/RepoList.jsx';
 import { fetchRepos } from '../../services/fetchRepos.js';
+import { githubApi } from '../../services/githubApi.js';
 
-export default class SearchUser extends Component {
+export default class UserContainer extends Component {
   
   state = {
     userName: '',
@@ -15,8 +15,8 @@ export default class SearchUser extends Component {
         fetchRepos()
           .then(repos => this.setState({ repos }));
       
-          githubApi()
-            .then(userName => this.setState({ userName }))
+        githubApi()
+          .then(userName => this.setState({ userName }));
         this.setState({ [target.name]: target.value });
       };
       
@@ -24,9 +24,9 @@ export default class SearchUser extends Component {
         const { userName, repos } = this.state;
         return (
           <>
-            <SearchUser username={ username } />
+            <SearchUser username={ userName } />
             <RepoList repos={ repos } />
-      </>
-    );
-  }
+          </>
+        );
+      }
 }
